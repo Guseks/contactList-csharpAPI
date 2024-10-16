1b0ab724fb8f15cbe84f8173484b1b42e16216e9
Gustaf Ekström <gusek93@gmail.com>
2024-10-16 17:31:35 +0200

    Refactor to use Controllers

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using contactList.Models;
+
+namespace contactList.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ContactsController : ControllerBase
+{
+  private static List<Contact> contacts = new List<Contact>();
+
+  public ContactsController()
+  {
+    if (contacts.Count == 0)
+    {
+      InitContacts();
+    }
+  }
+
+  [HttpGet]
+  public ActionResult<List<Contact>> GetAllContacts()
+  {
+    return Ok(contacts);
+  }
+
+  [HttpPost("new")]
+  public ActionResult AddNewContact(Contact contact)
+  {
+    contacts.Add(contact);
+    return Created($"Contact with name {contact.Name} created!", contact);
+  }
+
+  private void InitContacts()
+  {
+    var names = new string[3] { "Erik", "James", "Thomas" };
+    var phoneNumbers = new string[3] { "0734-124102", "0721-128923", "0737-828191" };
+    var emails = new string[3] { "test@gmail.com", "test2@gmail.com", "test3@gmail.com" };
+
+    for (var i = 0; i < names.Length; i++)
+    {
+      contacts.Add(new Contact(names[i], phoneNumbers[i], emails[i]));
+    }
+  }
+}
+
+
+
+
+
--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,16 +1,16 @@
-﻿namespace contactList;
+﻿namespace contactList.Models;
 
 public class Contact
 {
-    public string Name {get; set;}
-    public string Email {get; set;}
-    public string PhoneNumber {get; set;}
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string PhoneNumber { get; set; }
 
-    public Contact(string name, string phoneNumber, string email)
-    {
-        Name = name;
-        PhoneNumber = phoneNumber;
-        Email = email;
-    }
+  public Contact(string name, string phoneNumber, string email)
+  {
+    Name = name;
+    PhoneNumber = phoneNumber;
+    Email = email;
+  }
 
 }
